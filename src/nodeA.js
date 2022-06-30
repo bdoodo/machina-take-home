9757afae1665933e3f0d3b12db7e47c0a26b16f7
bdoodo <brianhdo@outlook.com>
2022-06-29 21:05:17 -0700

    prompt for user path to stl input

--- src/nodeA.js
+++ src/nodeA.js
@@ -1,36 +1,53 @@
 import WebSocket from 'ws'
 import fetch from 'node-fetch'
 import fs from 'fs'
-import path from 'path';
-import { fileURLToPath } from 'url';
-
-const __dirname = path.dirname(fileURLToPath(import.meta.url));
+import prompt from 'prompt'
 
 const ws = new WebSocket('ws://localhost:3000')
 
 ws.on('open', () => {
-  console.log('node A connected to web socket')
+  sendFile()
+})
+
+ws.on('error', () => {
+  console.log('Could not connect to web socket.')
 })
 
 // receive file data and save
-ws.on('message', function(data) {
-  console.log(data.byteLength)
-  fs.writeFile('output.stl', data, console.error)
+ws.on('message', function (data) {
+  fs.writeFile('output.stl', data, (err) => {
+    if (err) throw new Error(err)
+
+    console.log('output saved to current folder!')
+  })
 })
 
-const filePath = path.join(__dirname, 'assets', 'cad_mesh.stl')
-const length = fs.statSync(filePath).size
-const readStream = fs.createReadStream(filePath)
+const getFilePath = new Promise(res => {
+  prompt.start()
 
-try {
-  fetch('http://localhost:3001', {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/sla',
-      'Content-Length': length
-    },
-    body: readStream
+  prompt.get(['enter the filepath to your stl input'], (err, result) => {
+    if (err) throw new error(err)
+
+    res(result['enter the filepath to your stl input'])
   })
-} catch(e) {
-  console.error(e)
+})
+
+async function sendFile() {
+  const filePath = await getFilePath
+
+  const length = fs.statSync(filePath).size
+  const readStream = fs.createReadStream(filePath)
+
+  try {
+    fetch('http://localhost:3001', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/sla',
+        'Content-Length': length
+      },
+      body: readStream
+    })
+  } catch (e) {
+    console.error(e)
+  }
 }
